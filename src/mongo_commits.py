526f54a414d01678837de68a4d2fbde473487bb8
gesteves91 <esteves.gean@gmail.com>
2018-12-24 19:19:02 -0200

    add two important files

--- src/mongo_commits.py
+++ src/mongo_commits.py
@@ -39,8 +39,6 @@ with open('total.csv', 'w') as outfile:
         body = getCommentBySha(sha)
 
         extracted_url = extractURL(commits['url'])
-        print(reposi)
-        print(extracted_url)
         repos = getReposInfo(reposi, extracted_url)
 
         if 'commit' in commits:
