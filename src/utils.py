fda75d9e8d8a4092dc094502fe0c30035fa8bf08
gesteves91 <esteves.gean@gmail.com>
2018-12-24 19:51:49 -0200

    testing

--- src/utils.py
+++ src/utils.py
@@ -58,9 +58,10 @@ def getReposInfo(cursor, url):
 
   dictionaryRepos = {}
 
+  print(cursor)
+
   for repos in cursor:
     if repos["url"] == url:
-      print(repos["name"])
       dictionaryRepos.update({"name": repos["name"]})
       dictionaryRepos.update({"full_name": repos["full_name"]})
       dictionaryRepos.update({"private": repos["private"]})
